56c97ace7d1f4f09ca935f7637bf0234027aecc4
Aeious <91735910+xxaeiousxx@users.noreply.github.com>
2022-03-30 09:50:46 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicThreading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "     - Thread Running -";
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            Thread threadB = new Thread(MyThreadClass.Thread1);
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            System.Diagnostics.Debug.WriteLine("- End of Thread");
+            label1.Text = "      - End Of Thread -";
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BasicThreading
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                System.Diagnostics.Debug.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
